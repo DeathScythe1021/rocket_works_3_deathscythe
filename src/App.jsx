53f88c4ddc4e49fd08df89cc737af0b2fc11c75a
DeathScythe1021 <jenny90yeh@gmail.com>
2025-10-24 14:39:08 +0800

    feat: add InvoicePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
-import Button from "./components/Btn.jsx";
+import Button from "./components/Btn.jsx"
 import Card from "./components/Card.jsx"
 import SmCard from "./components/SmCard.jsx"
 import ProductPage from "./pages/ProductPage.jsx"
 import LoginPage from './pages/LoginPage.jsx'
-import ShippingPage from './pages/Shipping.jsx';
-import FinishPage from './pages/FinishPage.jsx';
-import ShoppingPage from './pages/ShoppingPage.jsx'
+import ShippingPage from './pages/Shipping.jsx'
+import FinishPage from './pages/FinishPage.jsx'
+import PayPage from './pages/PayPage.jsx'
+import InvoicePage from './pages/Invoice.jsx'
+
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -17,7 +19,7 @@ function App() {
     <>
     
       <Navbar />
-      <FinishPage />
+      <InvoicePage />
       <Footer />
     
     </>
--- src/pages/PayPage.jsx
+++ src/pages/PayPage.jsx
@@ -2,57 +2,90 @@ import React from "react";
 import Button from "../components/Btn";
 import p1 from "../assets/images/p-1.png";
 
-function ShippingPage() {
+function PayPage() {
   return (
     <main className="flex lg:py-[65px] lg:px-[122px] gap-5 justify-center">
-      <section className=" bg-primary-600 w-full flex flex-col justify-between items-center lg:w-[460px] ">
+      <section className=" bg-primary-600 w-full flex flex-col justify-between items-center lg:w-[460px]">
         <div className="p-[30px] w-full">
           <div className="flex w-full justify-between items-center pb-[30px]">
-            <h1 className="h1-text ">運送</h1>
+            <h1 className="h1-text ">付款</h1>
             {/* 三顆點點 */}
-
-            <div className="flex justify-between items-center relative w-[190px] h-[1px] bg-primary-400">
-              <div className="flex justify-center items-center top-[-10px] w-5 h-5 border-1 border-white rounded-full bg-primary-600 ">
-                <div className="w-3 h-3  bg-white rounded-full"></div>
-              </div>
-              <div className="flex justify-center items-center top-[-10px] w-5 h-5 border-1 border-primary-400 rounded-full bg-primary-600 "></div>
-
-              <div className="flex justify-center items-center top-[-10px] w-5 h-5 border-1 border-primary-400 rounded-full bg-primary-600 "></div>
+            
+          <div className="flex justify-between items-center relative w-[190px] h-[1px] bg-primary-400">
+            <div className="flex justify-center items-center top-[-10px] w-5 h-5 border-1 border-white rounded-full bg-white ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#3F5D45"
+              >
+                <path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z" />
+              </svg>
             </div>
+            <div className="flex justify-center items-center top-[-10px] w-5 h-5 border-1 border-white rounded-full bg-primary-600 ">
+              <div className="w-3 h-3  bg-white rounded-full"></div>
+            </div>
+            
+            <div className="flex justify-center items-center top-[-10px] w-5 h-5 border-1 border-primary-400 rounded-full bg-primary-600 ">
+            </div>
+            </div>
+           
           </div>
           {/* 個資 */}
           <div className="flex flex-col gap-4">
             <div className="flex gap-[9px] w-full">
               <div className="flex flex-col w-full gap-2">
-                <p className="money-text text-white">姓名</p>
-                <div className="flex justify-start text w-full py-[17px] px-[20px] bg-primary-100 text-primary-400">
-                  王
-                </div>
-              </div>
-              <div className="flex flex-col w-full gap-2">
-                <p className="money-text text-white">名字</p>
-                <div className="flex justify-start text w-full py-[17px] px-[20px] bg-primary-100 text-primary-400">
-                  小明
+                <p className="money-text text-white">信用卡卡號</p>
+                <div className="flex justify-between text w-full py-[17px] px-[20px] bg-primary-100 text-primary-400">
+                  <p>9012-3456-7890-1234</p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#3F5D45"
+                  >
+                    <path d="M864-696v432q0 29-21.15 50.5T792-192H168q-29 0-50.5-21.5T96-264v-432q0-29 21.5-50.5T168-768h624q29.7 0 50.85 21.5Q864-725 864-696Zm-696 72h624v-72H168v72Zm0 144v216h624v-216H168Zm0 216v-432 432Z" />
+                  </svg>
                 </div>
               </div>
             </div>
-            <div className="flex flex-col w-full gap-2">
-              <p className="money-text text-white">電話</p>
-              <div className="flex justify-start text w-full py-[17px] px-[20px] bg-primary-100 text-primary-400">
-                0912-345-678
-              </div>
-            </div>
+
             <div className="flex gap-[9px] w-full">
               <div className="flex flex-col w-full gap-2">
-                <p className="money-text text-white">地址</p>
+                <p className="money-text text-white">姓名</p>
+                <div className="flex gap-2">
+                  <div className="flex justify-start text w-full py-[17px] px-[20px] bg-primary-100 text-primary-400">
+                    王
+                  </div>
+                  <div className="flex justify-start text w-full py-[17px] px-[20px] bg-primary-100 text-primary-400">
+                    小明
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex gap-[9px] w-full">
+              <div className="flex flex-col w-full gap-2">
+                <p className="money-text text-white">有效期限</p>
                 <div className="flex  w-full gap-2">
                   <div className="relative w-full">
-                    <select className="w-full py-[17px] px-[20px] bg-primary-100 text-primary-400 appearance-none ">
-                      <option value="kaohsiung">高雄市</option>
-                      <option value="taipei">臺北市</option>
-                      <option value="taichung">臺中市</option>
-                      <option value="tainan">臺南市</option>
-                      <option value="taoyuan">桃園市</option>
+                    <select
+                      defaultValue=""
+                      className="w-full py-[17px] px-[20px] bg-primary-100 text-primary-400 appearance-none"
+                    >
+                      {/* 提示字，只顯示在未選時，不會出現在展開選單 */}
+                      <option value="" disabled hidden>
+                        月
+                      </option>
+
+                      {/* 月份選項 */}
+                      {Array.from({ length: 12 }, (_, i) => (
+                        <option key={i + 1} value={i + 1}>
+                          {i + 1}
+                        </option>
+                      ))}
                     </select>
                     <div className="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2">
                       <svg
@@ -67,12 +100,21 @@ function ShippingPage() {
                     </div>
                   </div>
                   <div className="relative w-full">
-                    <select className="w-full py-[17px] px-[20px] bg-primary-100 text-primary-400 appearance-none ">
-                      <option value="xinxing">新興區</option>
-                      <option value="qianjin">前金區</option>
-                      <option value="lingya">苓雅區</option>
-                      <option value="yancheng">鹽埕區</option>
-                      <option value="zuoying">左營區</option>
+                    <select
+                      defaultValue=""
+                      className="w-full py-[17px] px-[20px] bg-primary-100 text-primary-400 appearance-none"
+                    >
+                      <option value="" disabled hidden>
+                        年
+                      </option>
+                      {Array.from({ length: 12 }, (_, i) => {
+                        const year = new Date().getFullYear() + i;  //new Date().getFullYear() 取得 今年的西元年
+                        return (        //在箭頭函式中回傳一個 JSX 元素，用以渲染下拉選項
+                          <option key={year} value={year}>
+                            {year}
+                          </option>
+                        );
+                      })}
                     </select>
                     <div className="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2">
                       <svg
@@ -87,9 +129,16 @@ function ShippingPage() {
                     </div>
                   </div>
                 </div>
-                <div className="flex justify-start text w-full py-[17px] px-[20px] bg-primary-100 text-primary-400">
-                  幸福路 520 號
+                 <div className="flex gap-[9px] w-full">
+              <div className="flex flex-col w-full gap-2">
+                <p className="money-text text-white">背後末三碼</p>
+                <div className="flex justify-between text w-[calc(50%-8px)] py-[17px] px-[20px] bg-primary-100 text-primary-400">
+                  <p>123</p>
                 </div>
+                
+              </div>
+            </div>
+
               </div>
             </div>
           </div>
@@ -151,4 +200,4 @@ function ShippingPage() {
   );
 }
 
-export default ShippingPage;
+export default PayPage;
